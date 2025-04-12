056480a23728db8d7b8640aa84c1e0fea490b6d2
selina <ss@ss.com>
2025-04-12 15:46:15 -0400

    printing calculator options and having user pick one

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,17 @@
 package com.ps;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("1) Mortgage Calculator");
+        System.out.println("2) future Value of CD Calculator");
+        System.out.println("Please select a calculator: ");
+        int givenCommand = scanner.nextInt();
+
+        System.out.println("You have selected: ");
     }
 }
+
