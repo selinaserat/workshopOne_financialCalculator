fd61e93315321e959b339c0637ba005a222699b6
selina <ss@ss.com>
2025-04-13 21:11:01 -0400

    readme committed

--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 # financial calculator
+This application implements two different calculators; a mortgage calculator and a calculator that finds the future value of a CD. The screen is prompted to select which calculator they would like to use. 
+![Screenshot 2025-04-13 201146.png](../../../Desktop/Screenshot%202025-04-13%20201146.png)
+![Screenshot 2025-04-13 202834.png](../../../Desktop/Screenshot%202025-04-13%20202834.png)
+![Screenshot 2025-04-13 202910.png](../../../Desktop/Screenshot%202025-04-13%20202910.png)
+One interesting piece of code is the switch, with the cases. It's interesting because as someone that had learned basic Java, including if statemtns and loops, I was never taught the switch method, which is way easier and more readable.
--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -16,28 +16,22 @@ public class Main {
         switch (given_command){
             case 1:
 //                M=P×(i*(1+i)^n / ((1+i)^n)-1)
-//                - M = Monthly Payment
-//                - P = Principal: This is the total amount of the loan.
-//                - r = Annual Interest Rate: The nominal annual interest rate in decimal form (e.g., 7.625% = 0.07625).
-//                - y = Loan Term in Years =  How many years the loan lasts.
-//                - n = Number of Monthly Payments: This is 12×y (Because there are 12 monthly payments per year.)
-//                - i = Monthly Interest Rate: This is the annual interest rate divided by 12, i.e. r/12
 //                - Total Interest = (M×n)−P
 
                 System.out.print("Principal loan amount: ");
-                int principal = scanner.nextInt();
+                int principal = scanner.nextInt(); // "P" in the formula
 
                 System.out.print("Annual interest rate (as a percentage, like 7.625): ");
-                double InterestRate = scanner.nextDouble();
+                double InterestRate = scanner.nextDouble(); // "r"
 
                 System.out.print("Loan length in years: ");
-                short loanLength = scanner.nextShort();
+                short loanLength = scanner.nextShort(); // "y"
 
                 InterestRate = (InterestRate / 100); //convert from percentage to decimal
-                int numberOfMonthlyPayments = (12 * loanLength);
-                double monthlyRate = (InterestRate / 12);
+                int numberOfMonthlyPayments = (12 * loanLength); // "n"
+                double monthlyRate = (InterestRate / 12); // "i"
 
-                double monthlyPayment = (principal * (monthlyRate * Math.pow(1+monthlyRate, numberOfMonthlyPayments)) / (Math.pow(1+monthlyRate, numberOfMonthlyPayments)-1));
+                double monthlyPayment = (principal * (monthlyRate * Math.pow(1+monthlyRate, numberOfMonthlyPayments)) / (Math.pow(1+monthlyRate, numberOfMonthlyPayments)-1)); // "M'
                 double totalInterest = (monthlyPayment * numberOfMonthlyPayments) - principal ;
 
                 System.out.printf("Expected monthly payment: $%.2f\n", monthlyPayment);
@@ -47,21 +41,16 @@ public class Main {
             case 2:
 //               FV = P × (1 + (r / 365))^(365 × t)
 //                 Future Value (FV)
-//                 Principal (P): This is the initial deposit amount.
-//                 Annual Interest Rate (r): The nominal annual interest rate in decimal form (e.g., 1.75% = 0.0175).
-//                 Number of Years (t): The total number of years the deposit will earn interest.
-//                 Days Per Year: Daily compounding assumes 365 days per year.
-//                 Total Number of Days: This is 365 × t (because there are 365 days per year).
 //                 Total Interest Earned = FV - P
 
                 System.out.print("Deposit amount: ");
-                double cdPrincipal = scanner.nextFloat();
+                double cdPrincipal = scanner.nextFloat(); // "P"
 
                 System.out.print("Annual interest rate (as a percentage, like 1.75): ");
-                double cdInterestRate = scanner.nextDouble();
+                double cdInterestRate = scanner.nextDouble(); // "r"
 
                 System.out.print("Number of years: ");
-                short cdYears = scanner.nextShort();
+                short cdYears = scanner.nextShort(); // "t"
 
                 cdInterestRate = cdInterestRate / 100;
                 double totalDays = 365 * cdYears;
